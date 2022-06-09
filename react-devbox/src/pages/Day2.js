a4ddf344c4e0a1996c566520ee3d9ca76c727d31
allenwong <allenwong@verizonmedia.com>
2022-06-08 23:03:14 -0700

    day2

--- react-devbox/src/pages/Day2.js
+++ react-devbox/src/pages/Day2.js
@@ -1,7 +1,37 @@
+import {useState} from 'react'
+
+export const RobotList = ({data}) =>{
+  console.log(data)
+  return data.map(imgUrl=>{
+    return (<div><img src={imgUrl} alt="" /></div>)
+  })
+}
+
 export const Day2= () => {
+    const [robotList, setRobotList] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const handleSubmit = (e)=>{
+      e.preventDefault()
+      setRobotList([...robotList,`https://robohash.org/${searchTerm}`])
+    }
+
+    const handleOnChange = (e)=>{
+      setSearchTerm(e.target.value)
+    }
+
     return (
-        <div style={{ margin: "auto", width: "70%", paddingTop: 40 }}>
-          <h1>Day2</h1>
+      <div style={{ margin: "auto", width: "70%", paddingTop: 40 }}>
+        <h1>Day2</h1>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input onChange={(e)=>handleOnChange(e)}/>
+            <button type='submit'>submit</button>
+          </form>
+          <div style={{display:'grid',gridTemplateColumns:'1fr 1fr 1fr'}}>
+            {robotList ? <RobotList data={robotList} /> : null }
+          </div>
         </div>
-      );
+      </div>
+    );
 }
