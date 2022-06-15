ef44555129b66888fdb125e7424c56550ac67d01
allenwong <allenwong@verizonmedia.com>
2022-06-14 22:31:30 -0700

    day6

--- react-devbox/src/index.js
+++ react-devbox/src/index.js
@@ -3,20 +3,22 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 import App from './App';
-import {Day1,Day2,Day3,Day4,Day5} from './pages';
+import {Day1,Day2,Day3,Day4,Day5,Day6} from './pages';
 import './index.css';
 
+const DAY_NUM = 6
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App days={5} />}>
+        <Route path="/" element={<App days={DAY_NUM} />}>
             <Route path="day1" element={<Day1 />} />
             <Route path="day2" element={<Day2 />} />
             <Route path="day3" element={<Day3 />} />
             <Route path="day4" element={<Day4 />} />
             <Route path="day5" element={<Day5 />} />
+            <Route path="day6" element={<Day6 />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- react-devbox/src/pages/Day6.js
+++ react-devbox/src/pages/Day6.js
@@ -1,7 +1,80 @@
+import { useState } from "react";
+
 export const Day6= () => {
+  const INITIAL_LIST = {
+    "Organize closet": [
+      { "Donate old clothes and shoes": false },
+      { "Buy new shelf": false },
+      { "Put in shelf by color": false },
+    ],
+    "Finish homework": [
+      { "Finish math homework": false },
+      { "Finish science homework": false },
+      { "Finish Reactjs homework": false },
+    ],
+    "Achieve nirvana": [
+      { "Meditate a little": false },
+      { "Gain some wisdom": false },
+    ],
+  };
+
+  const [todoList,setToDoList] = useState(INITIAL_LIST)
+
+  const handleToggleToDo = (key,todoIdx)=>{
+    const todoKey = Object.keys(todoList[key][todoIdx])[0]
+    const newList = Object.assign({},todoList)
+    console.log(key,todoKey,newList[key][todoIdx][todoKey])
+
+    newList[key][todoIdx][todoKey] = !newList[key][todoIdx][todoKey]
+    console.log(newList)
+    setToDoList(newList)
+  }
+
+  const ToDoList = ({todo,title})=>{
+    
+    const Clist = ()=>{
+      return todo.map((task,idx)=>{
+        const key = Object.keys(task)
+        if(task[key]) return <p onClick={()=>handleToggleToDo(title,idx)}>{key}</p>
+        return null
+      })
+    }
+
+      const Ilist = ()=>{
+        return todo.map((task,idx)=>{
+          const key = Object.keys(task)
+          if(!task[key]) return <p onClick={()=>handleToggleToDo(title,idx)}>{key}</p>
+          return null
+        })
+      }
+
+    return(
+      <>
+        <h2>{title}</h2>
+        <div>
+          <div>
+            <h2>Completed</h2>
+            <ul>
+              <Clist/>
+            </ul>
+          </div>
+          <div>
+            <h2>Incompleted</h2>
+            <ul>
+              <Ilist/>
+            </ul>
+          </div>
+        </div>
+      </>
+    )
+  }
+
     return (
       <>
-        <h1>Day6</h1>
+        {/* loop thru list */}
+        {
+          Object.keys(todoList).map(todoData=><ToDoList todo={todoList[todoData]} title={todoData} />)
+        }
       </>
     );
 }
